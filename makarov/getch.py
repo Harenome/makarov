5a33a08394c70e5e9ddcbd61c5fa4a0938395cdc
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2014-05-30 16:20:45 +0200

    :book: complete documentation

--- makarov/getch.py
+++ makarov/getch.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://www.wtfpl.net/ for more details.
+
 #get character
 class _Getch:
+    """Class to get a character."""
     def __init__(self):
+        """Init."""
         import tty, sys
 
     def __call__(self):
+        """Call."""
         import sys, tty, termios
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
