7974961dc4d2d4d1f8d77bcb7af74137675efea1
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2014-05-30 15:46:18 +0200

    :metal: add backspace support

--- makarov/markov_time.py
+++ makarov/markov_time.py
@@ -5,44 +5,89 @@ import sys
 from markov_chain import MarkovChain
 from getch import getch
 
-def __read_times():
-    """Read times between keystrokes and save them in a Markov chain."""
-    chain = MarkovChain()
-    a = getch()
-    sys.stdout.write(a)
-    while a != '\r':
+class MarkovTimeReader:
+    """Class to read times between keystrokes and return a Markov chain."""
+    def __init__(self):
+        """Init."""
+        self.__chain = MarkovChain()
+        self.__text = list()
+        self.__times = list()
+
+    def __read_character(self):
+        """Read a character and return it along with a time."""
         start = time.time()
-        b = getch()
+        input_char = getch()
         end = time.time()
         t = end - start
-        chain.add_value(a, b, t)
-        a = b
-        sys.stdout.write(a)
-    sys.stdout.write('\n')
 
-    return chain
+        return input_char, t
 
-def compare_users():
+    def __backspace(self):
+        """When the input character is a backspace."""
+        if self.__text:
+            sys.stdout.write('\b \b')
+            if len(self.__text) > 1:
+                previous = self.__text.pop()
+                ante_previous = self.__text[-1]
+                old_time = self.__times.pop()
+                self.__chain.add_value(ante_previous, previous, - old_time)
+
+    def __normal_character(self, input_character, interval):
+        """When the input character is a normal character."""
+        if self.__text:
+            previous = self.__text[-1]
+            self.__chain.add_value(previous, input_character, interval)
+            self.__times.append(interval)
+
+    def read(self):
+        """Read. The object is reset."""
+        # Reset the object.
+        self.__init__()
+
+        go_on = True
+        while go_on:
+            input_character, interval = self.__read_character()
+            go_on = input_character != '\r'
+            if go_on:
+                if input_character == '\x7f':
+                    self.__backspace()
+                else:
+                    self.__normal_character(input_character, interval)
+                    self.__text.append(input_character)
+                    sys.stdout.write(input_character)
+
+        sys.stdout.write('\n')
+
+        return self.__chain
+
+def compare_users(verbose):
     """Compare two users."""
     USER_1 = "Bro 1"
     USER_2 = "Bro 2"
     COMPARISON = " != "
 
-    MarkovChain.set_epsilon = 0.1
+    MarkovChain.set_epsilon = 0.3
 
+    reader = MarkovTimeReader()
     print USER_1
-    chain_1 = __read_times()
+    chain_1 = reader.read()
     print USER_2
-    chain_2 = __read_times()
+    chain_2 = reader.read()
     if MarkovChain.are_similiar(chain_1, chain_2):
         COMPARISON = " == "
 
     print "\n" + USER_1 + COMPARISON + USER_2
-    print USER_1 + ":"
-    chain_1.display()
-    print USER_2 + ":"
-    chain_2.display()
+
+    if verbose:
+        print "\nMarkov chains:"
+        print USER_1 + ":"
+        chain_1.display()
+        print USER_2 + ":"
+        chain_2.display()
 
 # Run.
 if __name__ == "__main__":
-    compare_users()
+    verbosity = False
+    if len(sys.argv) > 1 and sys.argv[1] == "--verbose":
+        verbosity = True
+    compare_users(verbosity)
